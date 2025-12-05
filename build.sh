0010958e4b9a08b03cc38166d6073d3df0c9a511
연은서 <yeoneunseo0720@yonsei.ac.kr>
2025-12-05 12:15:35 +0900

    chore: Update build script and GitHub Actions workflow for improved deployment process

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
+#!/bin/bash
+# Build script to prepare project files for deployment
+# Note: This script is not used by GitHub Actions workflow
+# The workflow has its own build steps
+
 cd ../
+# Remove existing output directory if it exists
+rm -rf output
 mkdir output
-cp -R ./gdgoc-opt-team-3-front/* ./output
-cp -R ./output ./gdgoc-opt-team-3-front/
+
+# Copy project files, excluding unnecessary directories
+cd gdgoc-opt-team-3-front
+find . -maxdepth 1 -not -path "./output" -not -path "./.git" -not -path "./.github" -not -path "./node_modules" -not -path "./dist" -exec cp -R {} ../output/ \;
 
